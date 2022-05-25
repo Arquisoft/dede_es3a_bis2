fbc2accbd71689495757acc9c187c7719fc234f9
Sofía Suárez Fernández <UO270149@uniovi.es>
2022-05-25 17:11:27 +0200

    cambiar footer

--- webapp/src/components/Fragments/Footer.tsx
+++ webapp/src/components/Fragments/Footer.tsx
@@ -3,27 +3,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from "@mui/material";
 
 const useStyles = makeStyles({
-    stickToBottom: {
-      backgroundColor:'#000000',
-      background: "linear-gradient(45deg, #000000 30%, #FFFFFF 70%)",
-      marginTop: '1rem',
-      padding: '1rem',
-      position: 'fixed',
-      bottom: '0',
-      left: '0',
-      width: '100%',
-      textAlign:'center'
-    },
+  stickToBottom: {
+    backgroundColor: '#000000',
+    background: "linear-gradient(45deg, #000000 30%, #FFFFFF 70%)",
+    marginTop: '1rem',
+    padding: '1rem',
+    position: 'fixed',
+    bottom: '0',
+    left: '0',
+    width: '100%',
+    textAlign: 'center'
+  },
 });
 
 const Footer = () => {
-    const classes = useStyles();
-    return (
-      <div className={classes.stickToBottom}>
-        <Typography variant='h6' >
-          @DeDe_es3A
-        </Typography>
-      </div>
-    );
+  const classes = useStyles();
+  return (
+    <div className={classes.stickToBottom}>
+      <Typography variant='h6' >
+        @dede_es3a_bis2
+      </Typography>
+    </div>
+  );
 };
 export default Footer;
